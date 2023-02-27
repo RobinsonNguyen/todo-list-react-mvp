635dc441aa92d7903b706999e46e9b31b564db8a
RobinsonNguyen <robinson_nguyen@hotmail.com>
2023-02-28 02:14:16 +0900

    changed fetch requests

--- client/src/App.js
+++ client/src/App.js
@@ -131,12 +131,14 @@ function DisplayTodoItem({ id, element, addTodos, removeTodos, currentDate }) {
   const [edit, setEdit] = useState(false);
 
   async function getData() {
-    const res = await fetch("http://localhost:3001/test");
+    const res = await fetch("https://todo-list-react-mvp.onrender.com/test");
     const data = await res.json();
     addTodos(data);
   }
   const handleGetOne = async (e) => {
-    const response = await fetch(`http://localhost:3001/test/${e.target.id}`);
+    const response = await fetch(
+      `https://todo-list-react-mvp.onrender.com/test/${e.target.id}`
+    );
     const data = await response.json();
     console.log(`I want to edit id: ${e.target.id}`);
     console.log(data[0]);
@@ -149,16 +151,19 @@ function DisplayTodoItem({ id, element, addTodos, removeTodos, currentDate }) {
       ? `${e.target[2].value}T${e.target[3].value}:00`
       : e.target[2].value;
 
-    const response = await fetch(`http://localhost:3001/test/${e.target.id}`, {
-      method: "PATCH",
-      body: JSON.stringify({
-        name: `${e.target[0].value}`,
-        title: `${e.target[1].value}`,
-        start: date,
-        modified: new Date(),
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
+    const response = await fetch(
+      `https://todo-list-react-mvp.onrender.com/test/${e.target.id}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({
+          name: `${e.target[0].value}`,
+          title: `${e.target[1].value}`,
+          start: date,
+          modified: new Date(),
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
     const data = await response.json();
     console.log(`updating on id: ${e.target.id}`);
     // const temp = {};
@@ -171,9 +176,12 @@ function DisplayTodoItem({ id, element, addTodos, removeTodos, currentDate }) {
     setEdit(!edit);
   };
   const handleDelete = async (e) => {
-    const response = await fetch(`http://localhost:3001/test/${e.target.id}`, {
-      method: "DELETE",
-    });
+    const response = await fetch(
+      `https://todo-list-react-mvp.onrender.com/test/${e.target.id}`,
+      {
+        method: "DELETE",
+      }
+    );
     const data = await response.json();
     console.log(`I am deleting id: ${e.target.id}`);
     console.log(data[0]);
@@ -251,7 +259,7 @@ function CreateNewTodo(props) {
   const [date, setDate] = useState(`${y}-${m}-${d}`);
 
   async function getData() {
-    const res = await fetch("http://localhost:3001/test");
+    const res = await fetch("https://todo-list-react-mvp.onrender.com/test");
     const data = await res.json();
     props.addTodos(data);
   }
@@ -265,13 +273,16 @@ function CreateNewTodo(props) {
       modified: new Date(),
     };
     const sendTodosToAPI = async () => {
-      const response = await fetch("http://localhost:3001/test", {
-        method: "POST",
-        body: JSON.stringify(obj),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        "https://todo-list-react-mvp.onrender.com/test",
+        {
+          method: "POST",
+          body: JSON.stringify(obj),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
       const data = await response.json();
       //sends first and only row from sql query to setTodo state
       // const temp = {};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 const postgres = require("postgres");
 const dotenv = require("dotenv");
 dotenv.config();
